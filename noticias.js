79ada0f9c26b69cc775e4056bedbc3bbf3d62016
TiagoChiaradia <97569978+TiagoChiaradia@users.noreply.github.com>
2022-05-09 21:06:03 -0300

    Add files via upload

---
+++ noticias.js
@@ -0,0 +1,25 @@
+console.log("Teste")
+var http = require('http'); //acresenta a biblioteca http
+
+const port = process.env.PORT ||3000;
+
+var server = http.createServer( function(req,res){
+	var categoria = req.url;
+if (categoria == '/tecnologia'){
+	res.end("<html><body>Portal de noticias de tecnologia</body></html>");
+} else if (categoria == '/moda'){
+	res.end("<html><body>Portal de noticias moda</body></html>");
+} else if (categoria == '/beleza'){
+
+	res.end("<html><body>Portal de noticias de beleza</body></html>");
+}else if (categoria == '/TI'){
+	res.end("<html><body>Portal de noticias de TI</body></html>");
+}
+else {
+    res.end("<html><body>Portal de noticias</body></html>");
+}
+});
+
+server.listen(port,()=> {
+	console.info("Aplicação Rodando !");
+});
