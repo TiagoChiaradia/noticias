39914c7da38eda27e798d0d81342a72590775f39
TiagoChiaradia <chiaradia@ymail.com>
2022-05-11 22:52:18 -0300

    module

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 var express = require('express'); //acresenta a biblioteca http
 var app=express();
 
+var msg = require('./mod_teste');
+
+app.set('view engine','ejs');
+
 
 app.get('/',function(req,res){
-    res.send("<html><body>Portal de noticias</body></html>");
+    res.render("home/index");
 })
 
-app.get('/tecnologia',function(req,res){
-    res.send("<html><body>Portal de noticias de tecnologia</body></html>");
+app.get('/formulario_inclusao_noticia',function(req,res){
+    res.render("admin/form_add_noticia");
+})
+
+
+
+app.get('/noticias',function(req,res){
+    res.render("noticias/noticias");
 })
 
 app.listen(3000,function(){
 
     console.log("Servidor Rodando");
+    console.log(msg());
+
 });
+
